3dea7309f476b4ec3c73f80934312eaf3311606b
J o s e p h  S a m i r <joseph.samir@msa.edu.eg>
2021-02-02 15:49:52 +0200

    Aded tailwind

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo1 from "./assets/coding.png"
 import logo2 from "./assets/binary-code.png"
 import logo3 from "./assets/code.png"
 import logo4 from "./assets/structure.png"
+import logo5 from "./assets/browser.png"
 function App() {
   
    
@@ -21,13 +22,15 @@ function App() {
             6: logo3,
             7: logo4,
             8: logo4,
+            9: logo5,
+            10: logo5,
             
             /* 3: your other image... */
             /* 4: your other image... */
             /* 5: your other image... */
             /* ... */
           }}
-          how={15} /* Pass the number of images Sky will render chosing randomly */
+          how={20} /* Pass the number of images Sky will render chosing randomly */
           time={10} /* time of animation */
           size={'100px'} /* size of the rendered images */
           background={'#263238'} /* color of background */
